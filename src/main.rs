887695e864741e3f0441184a03f35ecec4ea56c5
Sam Keen <sam.sjk@gmail.com>
2024-02-04 19:19:46 -0800

    completed all three commands

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use chrono::Utc;
 use hex;
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
-use std::io::{Result, Write};
+use std::process::exit;
 use std::{env, fs};
+
+const PERSIST_FILE_PATH: &str = "/todo.json";
 
 fn generate_id() -> String {
     let current_time = Utc::now().to_string();
@@ -21,39 +23,76 @@ struct Todo {
     is_complete: bool,
 }
 
-fn get_existing_todos() -> Result<Vec<Todo>> {
-    let file_content = fs::read_to_string("../todo.json").expect("Could not read file");
-    let todos: Vec<Todo> = serde_json::from_str(&file_content)?;
-    Ok(todos)
+fn get_existing_todos() -> Vec<Todo> {
+    /*
+      This next line is equivalent to this match expression:
+        let existing_todos_serialized = match fs::read_to_string(PERSIST_FILE_PATH) {
+            Ok(content) => content,
+            Err(_) => "[]".to_string(),
+        };
+    */
+    let existing_todos_serialized =
+        fs::read_to_string(PERSIST_FILE_PATH).unwrap_or_else(|_| "[]".to_string());
+    return serde_json::from_str(&existing_todos_serialized).unwrap_or_else(|_| Vec::new());
 }
 fn persist_todos(todos: Vec<Todo>) {
     let serialized = serde_json::to_string(&todos).expect("Failed to serialize data");
-    fs::write("../todo.json", serialized).expect("Could not write to file");
+    fs::write(PERSIST_FILE_PATH, serialized).expect("Could not write to file");
 }
 
 fn add_todo(text: &str) {
-    let mut todo = Todo {
+    let todo = Todo {
         id: generate_id(),
         text: text.to_string(),
         is_complete: false,
     };
-    let mut existing_todos = match get_existing_todos() {
-        Ok(todos) => todos,
-        Err(e) => panic!("failed to retrieve existing todos: {}", e),
-    };
+    let todo_id = todo.id.clone();
+    let mut existing_todos = get_existing_todos();
     existing_todos.push(todo);
-    persist_todos(existing_todos)
+    persist_todos(existing_todos);
+    println!("Added Todo[{}] '{}'", todo_id, text);
 }
 
-fn list_todos() {}
+fn list_todos() {
+    println!("== Existing todos ==");
+    let existing_todos = get_existing_todos();
+    if existing_todos.len() == 0 {
+        println!("There are no saved Todos");
+        exit(0)
+    }
+    for todo in existing_todos {
+        println!("[{}] {}", todo.id, todo.text)
+    }
+}
 
-fn remove_todo(todo_id: &str) {}
+fn remove_todo(todo_id: &str) {
+    let mut existing_todos = get_existing_todos();
+    match existing_todos.iter().find(|&todo| todo.id == todo_id) {
+        Some(todo_to_remove) => {
+            println!(
+                "Removed Todo[{}] '{}'",
+                todo_to_remove.id, todo_to_remove.text
+            );
+            existing_todos.retain(|todo| todo.id != todo_id);
+            persist_todos(existing_todos);
+        }
+        None => {
+            println!("Todo with id [{}] was not found", todo_id)
+        }
+    };
+}
 
 fn main() {
     // read the commandline args
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        panic!("USAGE: no command given")
+        println!("No command given");
+        println!("USAGE: todo <command> [arg]");
+        println!("Known commands:");
+        println!("todo add \"todo description\"");
+        println!("todo list");
+        println!("done <id of todo>");
+        exit(0)
     }
 
     let command = args[1].as_str();
